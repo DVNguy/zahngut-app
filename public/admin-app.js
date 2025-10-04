1a5e20962e63c9d0dca74ddddf015399be7d6e9f
DVNguy <relocation1xx@gmail.com>
2025-10-04 11:55:32 +0200

    Updated admin-app.js

--- public/admin-app.js
+++ public/admin-app.js
@@ -1,17 +1,20 @@
-import { auth, onAuthStateChanged, signOut } from './firebase-auth.js';
-import { db } from './firebase-db.js';
-import {
-  collection,
-  getDocs,
-  doc,
-  getDoc,
-  setDoc,
-  updateDoc,
-  deleteDoc,
-  query,
-  orderBy,
-  where
-} from 'firebase/firestore';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
+import { getAuth, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
+import { getFirestore, collection, getDocs, doc, getDoc, setDoc, updateDoc, deleteDoc, query, orderBy, where } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBci-nWGvx7OdMb6BmWbS8pWBR9Leidl_Q",
+  authDomain: "zahngut-app.firebaseapp.com",
+  databaseURL: "https://zahngut-app-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "zahngut-app",
+  storageBucket: "zahngut-app.firebasestorage.app",
+  messagingSenderId: "1022498780184",
+  appId: "1:1022498780184:web:922e819956feb594037f6d"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 class AdminPanel {
   constructor() {
