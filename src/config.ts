843d8125fee2a634990130bc8cdf8885bfbcadcb
DVNguy <relocation1xx@gmail.com>
2025-10-04 09:42:05 +0200

    Updated config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,9 @@
 const supabaseUrl =
+  import.meta.env.VITE_Bolt_Database_URL ||
   import.meta.env.VITE_SUPABASE_URL || '';
 
 const supabaseAnonKey =
+  import.meta.env.VITE_Bolt_Database_ANON_KEY ||
   import.meta.env.VITE_SUPABASE_ANON_KEY || '';
 
 const hasValidConfig = !!(supabaseUrl && supabaseAnonKey);
